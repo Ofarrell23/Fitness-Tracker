677ae4081b5f9195a556268d23ad43e56999aa42
Ofarrell23 <keyabug@gmail.com>
2021-05-26 21:38:38 -0700

    deploying

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // If deployed on heroku, use the deployed database. Otherwise use the local workout database
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workouts';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Fitness-Tracker';
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
